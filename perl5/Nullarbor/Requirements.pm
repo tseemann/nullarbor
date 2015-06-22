e04e18e19bd9339d8d9d1589b9bbbfd30f6b04c7
Torsten Seemann <torsten.seemann@gmail.com>
2015-06-22 14:43:14 +1000

    Add ability to check for min/max version of external tools

--- perl5/Nullarbor/Requirements.pm
+++ perl5/Nullarbor/Requirements.pm
@@ -1,10 +1,24 @@
 package Nullarbor::Requirements;
 
 use base Exporter;
-@EXPORT_OK = qw(require_exe require_perlmod);
+@EXPORT_OK = qw(require_exe require_perlmod require_version);
 
 use File::Spec;
 use Nullarbor::Logger qw(err msg);
+
+#----------------------------------------------------------------------
+sub require_version {
+  my($exe, $minver, $maxver) = @_;
+  err("missing minver or maxver parameter") unless $minver || $maxver;
+  my($line) = qx"$exe --version 2>&1";
+  chomp $line;
+  $line =~ m/\b(\d+(\.\d+)?)/;
+  my $ver = $1 or err("Could not determine $exe version: $line");
+  msg("Parsed version '$ver' from '$line'");
+  err("Need $exe >= $minver (found $ver)") if defined $minver && $ver < $minver;
+  err("Need $exe <= $maxver (found $ver)") if defined $maxver && $ver > $maxver;
+  return;
+}
 
 #----------------------------------------------------------------------
 sub require_exe {
