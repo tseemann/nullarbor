fa97a735f8131d9508d9c3435f3a9346c0b3b324
Monolith0 root <root@Monolith0.edlb.cdc.gov>
2015-09-15 17:00:59 -0400

    using wgsim instead of custom perl script for simulated data

--- test/make_test_data.sh
+++ test/make_test_data.sh
@@ -8,7 +8,8 @@ rm -f $TAB
 
 for F in $DIR/*.fa ; do
 	N=$(basename $F .fa)
-	fq-simulate_illumina_reads.pl --ref $F --prefix $DIR/$N --indels --ambigs
+	#fq-simulate_illumina_reads.pl --ref $F --prefix $DIR/$N --indels --ambigs
+	wgsim $F data/${N}_R1.fastq data/${N}_R2.fastq > /dev/null
 	echo -e "$N\t$DIR/${N}_R1.fastq\t$DIR/${N}_R2.fastq" >> $TAB
 	cp -f $F $REF 
 done
