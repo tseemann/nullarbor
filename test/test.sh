616369400c59ae1b82028a7a72ec67ffc34ca9f4
Torsten Seemann <torsten.seemann@gmail.com>
2018-05-09 20:10:50 +1000

    Initial test.sh script

--- test/test.sh
+++ test/test.sh
@@ -5,6 +5,6 @@ OUTDIR=nullarbor
 
 rm -fr ./$OUTDIR
 
-nullarbor.pl --ref $REF --input input.tab --outdir $OUTDIR --name Nullarbor-Test --force && nice make -j 4 -C $OUTDIR
-
+nullarbor.pl --ref $REF --input input.tab --outdir $OUTDIR --name Nullarbor-Test --force \
+	  && nice make -j 4 -C $OUTDIR
 
