5382efe7065ff7e053d8c7068efa55098c3f8769
Torsten Seemann <torsten.seemann@gmail.com>
2018-05-09 20:09:05 +1000

    V2 | Fix prokka parsing issue

--- perl5/Nullarbor/Module/assembly.pm
+++ perl5/Nullarbor/Module/assembly.pm
@@ -28,6 +28,21 @@ sub extract_insert_size {
 
 #...........................................................................................
 
+sub gff_ftype_count {
+  my($gff) = @_;
+  my %count;
+  open my $GFF, '<', $gff;
+  while (<$GFF>) {
+    next if m/^#/;  # header - comments
+    last if m/^>/;  # footer - FASTA
+    my(undef,undef,$ftype) = split m/\t/;
+    $count{$ftype}++ if defined $ftype;
+  }
+  return \%count;
+}
+
+#...........................................................................................
+
 sub html {
   my($self) = @_;
   my $indir = $self->indir;
@@ -38,7 +53,7 @@ sub html {
   map { $_->[0] =~ s{/contigs.fa}{} } @$ass;
   
   # extract insert size from BWA output in Snippy folder
-  push @{$ass->[0]}, "Insert size (25,50,75)%";
+###  push @{$ass->[0]}, "Insert size (25,50,75)%";
   my @annofeat = qw(CDS rRNA tRNA tmRNA);
   push @{$ass->[0]}, @annofeat;
   push @{$ass->[0]}, "Quality";
@@ -48,19 +63,19 @@ sub html {
   
   for my $row (1 .. @$ass-1) {
     my $id = $ass->[$row][0];
-    # embed BWA MEM insert size results
-    push @{ $ass->[$row] }, extract_insert_size("$indir/$id/$id/snps.log");
+###    # embed BWA MEM insert size results (v1 vs v2)
+###    my $snplog = -r "$indir/$id/snps.log" ? "$indir/$id/snps.log" : "$indir/$id/$id/snps.log";
+###    push @{ $ass->[$row] }, extract_insert_size($snplog);
     # embed prokka results in table
     my $prokka = "$indir/$id/prokka/$id";
-    if (-r "$prokka.txt") {
+    if (-r "$prokka.txt") {   # version 1.x
       my %anno = (map { ($_->[0] => $_->[1]) } @{ Nullarbor::Tabular::load(-file=>"$prokka.txt", -sep=>': ') } );
       push @{ $ass->[$row] }, (map { $anno{$_} } @annofeat);
     }
-    elsif (-r "$prokka.gff") {
+    elsif (-r "$id/contigs.gff") {   # version 2.x
+      my $count = gff_ftype_count("$id/contigs.gff");
       for my $ftype (@annofeat) {
-        my($count) = qx(grep -c -w '$ftype' '$prokka.gff');
-        chomp $count;
-        push @{ $ass->[$row] }, $count;
+        push @{ $ass->[$row] }, $count->{$ftype} || 0;
       }
     }
     # final traffic light
