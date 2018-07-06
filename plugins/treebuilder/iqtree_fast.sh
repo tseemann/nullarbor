b15e4cc5b0405bad308b9a60902d3735d0b72092
Torsten Seemann <torsten.seemann@gmail.com>
2018-07-06 18:01:35 +1000

    Issue #174 | add bootstrap labels to iqtree

--- plugins/treebuilder/iqtree_fast.sh
+++ plugins/treebuilder/iqtree_fast.sh
@@ -4,5 +4,5 @@ base="$( cd "$( dirname "$0" )" && pwd )"
 . "$base/../common.inc"
 . "$base/common.inc"
 
-iqtree -s "$aln" -redo -ntmax "$cpus" -st DNA -fast -m GTR+G4 $opts
+iqtree -s "$aln" -redo -ntmax "$cpus" -st DNA -fast -m GTR+G4 -bb 1000 -alrt 1000 $opts
 mv "$aln.treefile" "$tree"
--- plugins/treebuilder/iqtree_slow.sh
+++ plugins/treebuilder/iqtree_slow.sh
@@ -5,5 +5,5 @@ base="$( cd "$( dirname "$0" )" && pwd )"
 . "$base/common.inc"
 
 # 
-iqtree -s "$aln" -redo -ntmax "$cpus" -st DNA $opts
+iqtree -s "$aln" -redo -ntmax "$cpus" -st DNA -bb 1000 -alrt 1000 $opts
 mv "$aln.treefile" "$tree"
