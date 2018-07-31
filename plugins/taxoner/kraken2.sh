36b66bef662c2a8833aaba6daa00a09cecaadd8d
Torsten Seemann <torsten.seemann@gmail.com>
2018-07-31 17:35:54 +1000

    Forgot to add kraken2.sh plugin

---
+++ plugins/taxoner/kraken2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+base="$( cd "$( dirname "$0" )" && pwd )"
+. "$base/../common.inc"
+. "$base/common.inc"
+
+# will use $KRAKEN_DEFAULT_DB
+
+kraken2 --threads "$cpus" --paired "$read1" "$read2" \
+	--memory-mapping --minimum-base-quality 13 \
+	--report - --output "$outfile"
