b72cd7c3675ed1edbcfe54a38bb104c4fe534550
Torsten Seemann <torsten.seemann@gmail.com>
2018-08-19 10:51:54 +1000

    Issue #186 | handle non-gz files for skesa

--- plugins/assembler/skesa.sh
+++ plugins/assembler/skesa.sh
@@ -4,13 +4,11 @@ base="$( cd "$( dirname "$0" )" && pwd )"
 . "$base/../common.inc"
 . "$base/common.inc"
 
-#WORKDIR=$(mktemp -d)
+gzipped=""
+if [[ $read1 =~ gz$ ]]; then
+    gzipped="--gz"
+fi
 
-skesa --use_paired_ends --gz --fastq "$read1,$read2" \
-	--cores "$cpus" --memory 16 \
+skesa --use_paired_ends $gzipped --fastq "$read1,$read2" \
+	--cores "$cpus" --memory 16 $opts \
 	--contigs_out "$outdir/contigs.fa"
-
-#cp -v -f "$WORKDIR/"contigs.{fa,gfa} "$outdir"
-#cat "$WORKDIR/"*.log > "$outdir/contigs.log"
-#rm -frv "$WORKDIR"
-
