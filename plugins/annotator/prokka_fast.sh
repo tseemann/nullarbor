45fd2026c51517ff6399c355544881215e7bdcc7
Torsten Seemann <torsten.seemann@gmail.com>
2018-10-23 15:04:07 +1100

    Add --noTRNA to prokka_fast

--- plugins/annotator/prokka_fast.sh
+++ plugins/annotator/prokka_fast.sh
@@ -8,11 +8,13 @@ WORKDIR=$(mktemp -d)
 
 # gffout="$(@)" gbkout="$(@D)/contigs.gbk" "contigs=$(<)" locustag="$(@D)" gcode="$(GCODE)" minlen="$(MIN_
 
+# We use --notrna because sometimes good CDS are masked by bad Aragorn calls
+
 PREFIX=prokka
 
 prokka --force --outdir "$WORKDIR" --prefix "$PREFIX" --locustag "$locustag" \
 	--gcode "$gcode" --mincontiglen "$minlen" --strain "$locustag" \
-	--fast \
+	--notrna --fast \
 	"$contigs"
 
 cp -v -f "$WORKDIR/$PREFIX.gff" "$gffout"
